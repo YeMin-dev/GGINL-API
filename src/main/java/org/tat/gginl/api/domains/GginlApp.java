384c24c7ac4786625171b55e540170d92489a61e
ASUS <pyaesonehein1141991@gmail.com>
2021-03-23 10:55:32 +0630

    Add ID, Common data to GGinlAPP

--- src/main/java/org/tat/gginl/api/domains/GginlApp.java
+++ src/main/java/org/tat/gginl/api/domains/GginlApp.java
@@ -2,11 +2,13 @@ package org.tat.gginl.api.domains;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.persistence.Version;
 
+import org.tat.gginl.api.common.CommonCreateAndUpateMarks;
 import org.tat.gginl.api.common.TableName;
 
 import lombok.Data;
@@ -16,11 +18,15 @@ import lombok.Data;
 @Data
 @Access(value = AccessType.FIELD)
 public class GginlApp {
-	
+
+	@Id
 	private String id;
 	private String appName;
-	
+
 	@Version
 	private int version;
+
+	@Embedded
+	private CommonCreateAndUpateMarks recorder;
 
 }
